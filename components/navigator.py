2f0a4e6d9cde7b5fefaa7a298e5ecc3a0fbf0206
Andres D. Vega B <av666591@fh-muenster.de>
2020-10-16 16:02:33 +0200

    First version of the Navigation

---
+++ components/navigator.py
@@ -0,0 +1,7 @@
+from kivy.lang import Builder
+from kivymd.uix.boxlayout import BoxLayout
+# Builder
+Builder.load_file('components/navigator.kv')
+
+class RootScreenManagement(BoxLayout):
+    pass
