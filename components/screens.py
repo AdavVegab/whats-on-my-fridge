8ee116181f8c8396e93632fed56d6dd1b3e98593
Andres D. Vega B <av666591@fh-muenster.de>
2020-10-20 21:31:51 +0200

    Simplifies the Navigation

--- components/screens.py
+++ components/screens.py
@@ -2,20 +2,38 @@ from kivy.lang import Builder
 from kivymd.uix.boxlayout import BoxLayout
 from kivymd.uix.bottomnavigation import MDBottomNavigationItem
 from kivy.uix.screenmanager import ScreenManager
+from kivymd.uix.screen import Screen
+
+# # Screen For Adding Items
+# class ItemDetail(Screen):
+#     pass
+# Builder.load_file('components/ingredient_screen.kv')
+
+# # Screen For the Dashboard
+# class DashboardsScreen(MDBottomNavigationItem):
+#     pass
+# Builder.load_file('components/dashboard_screen.kv')
+
+# # Screen For the Ingredients
+# class IngredientsScreen(MDBottomNavigationItem):
+#     def add_items(self):
+#         self.manager.current = 'item_detail'        
+# Builder.load_file('components/ingredients_screen.kv')
+
+from kivymd.uix.navigationdrawer import NavigationLayout
+from kivy.properties import ObjectProperty
 
 
-# Screen For the Dashboard
-Builder.load_file('components/dashboard_screen.kv')
-class DashboardsScreen(MDBottomNavigationItem):
-    pass
-
-# Screen For the Ingredients
-Builder.load_file('components/ingredients_screen.kv')
-class IngredientsScreen(MDBottomNavigationItem):
-    pass
-
+class ContentNavigationDrawer(BoxLayout):
+    screen_manager = ObjectProperty()
+    nav_drawer = ObjectProperty()
+    
 # Navigator Control
-Builder.load_file('components/navigator.kv')
-class RootScreenManagement(BoxLayout):
-    pass
+class RootScreenManagement(NavigationLayout):
+    def __init__(self):
+        super().__init__()
+    def add_items(self):
+        self.ids.screen_manager.current = 'item_detail'
+        
+Builder.load_file('components/screens.kv')
 
--- main.py
+++ main.py
@@ -3,28 +3,15 @@ from kivymd.app import MDApp
 # Components
 from components.ingredient_card import IngredientCard
 from components.screens import RootScreenManagement
-from components.add_ingredient import AddIngredientPopUp, EditIngredientPopUp
 
 # Builder
 
 class WhatsOnMyFridge(MDApp):
     def __init__ (self):
         super().__init__()
-        self.add_popup = AddIngredientPopUp()
-        self.edit_popup = EditIngredientPopUp()
     def build(self):
         self.manager = RootScreenManagement()
         return self.manager
-    
-    def add_item(self):
-        self.manager.ids.ingredients.ids.md_list.add_widget(IngredientCard(self.add_popup.content_cls.ids.input.text))
-        self.add_popup.content_cls.ids.input.text = ''
-        self.add_popup.dismiss()
-
-
-        
-    def delete_ingredient(self, ingredient):
-        self.manager.ids.ingredients.ids.md_list.remove_widget(ingredient)
 
     def sync_dashboard(self):
         pass
