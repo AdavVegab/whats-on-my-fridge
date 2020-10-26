e475ebfd82dcf987a450efae94f15006aafcc4e1
Andres D. Vega B <av666591@fh-muenster.de>
2020-10-26 18:00:03 +0100

    Adds documentation to the components

--- components/ingredient_card.py
+++ components/ingredient_card.py
@@ -1,9 +1,15 @@
+"""
+KivyMD Wigdet Class for the ingredients
+"""
 from kivymd.uix.card import MDCard
 from kivy.lang import Builder
-from kivy.properties import StringProperty, ObjectProperty
+from kivy.properties import StringProperty
 
 Builder.load_file('components/ingredient_card.kv')
 class IngredientCard(MDCard):
+    """
+    MDCard containing all the relevant info of the ingredient
+    """
     text = StringProperty ('TEST')
     ingredient_id = 111
     def __init__(self, text, db_id, image):
--- components/recipe_card.py
+++ components/recipe_card.py
@@ -1,3 +1,6 @@
+"""
+KivyMD Wigdet Class for the diferents cards relating to the recipes
+"""
 from kivymd.uix.card import MDCard
 from kivymd.uix.chip import MDChip
 from kivy.lang import Builder
@@ -6,12 +9,18 @@ from kivy.properties import StringProperty, ObjectProperty
 Builder.load_file('components/recipe_card.kv')
 
 class IngredientChip(MDChip):
+    """
+    MDChip for the missing or avaiable ingredients
+    """
     text = StringProperty()
     def __init__(self, label):
         self.text = label
         super().__init__()
         
 class RecipeCard(MDCard):
+    """
+    MDCard containing a overview of the Recipe (for the Dashboard)
+    """
     text = StringProperty ()
     def __init__(self, recipe):
         super().__init__()
@@ -45,6 +54,9 @@ class RecipeCard(MDCard):
 
 
 class RecipeSummaryCard(MDCard):
+    """
+    MDCard containing all the relevant info of the Recipe
+    """
     text = StringProperty ()
     source_img = StringProperty()
     def __init__(self, recipe):
@@ -75,6 +87,9 @@ class RecipeSummaryCard(MDCard):
             self.ids.chip_layout_aviable.add_widget(chip)
 
 class RecipeStepCard(MDCard):
+    """
+    MDCard containing a step to complete the recipe
+    """
     text = StringProperty ()
     def __init__(self, step):
         super().__init__()
