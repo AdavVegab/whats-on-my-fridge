038eb72e2dd4cbd000a755f1d68501b199657e59
Andres D. Vega B <av666591@fh-muenster.de>
2020-10-29 14:07:29 +0100

    beautifies the Ingredient Cards

--- components/ingredient_card.py
+++ components/ingredient_card.py
@@ -2,24 +2,28 @@
 KivyMD Wigdet Class for the ingredients
 """
 from kivymd.uix.card import MDCard
+from kivymd.uix.imagelist import SmartTile
 from kivy.lang import Builder
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, NumericProperty
 
 Builder.load_file('components/ingredient_card.kv')
 class IngredientCard(MDCard):
     """
     MDCard containing all the relevant info of the ingredient
     """
-    text = StringProperty ('TEST')
-    ingredient_id = 111
-    def __init__(self, text, db_id, image):
+    label = StringProperty()
+    image_url = StringProperty()
+    spoonacular_id = NumericProperty()
+    
+    def __init__(self, label, spoonacular_id, image):
+        # Initialize Values
+        self.label = label.capitalize()
+        self.spoonacular_id = spoonacular_id
+        self.image_url = image
+        
+        if 'unknown' in self.image_url:
+            self.image_url = 'assets/images/unknown.png'
+        
         super().__init__()
-        self.text = text
-        self.db_id = db_id
-        self.image = image
-        if 'unknown' not in self.image:
-            self.ids.async_img.source = self.image
-        else:
-            self.ids.async_img.source = 'assets/images/unknown.png'
             
         
